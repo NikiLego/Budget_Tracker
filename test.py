39893c5169cbde0eca0251bc77312c028e55abcc
NikiLego <nikitha.jiji@stonybrook.edu>
2025-05-23 18:06:14 -0400

    Skeleton code

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 print("Hello, world!")
 
+Salary = float(input("Enter your current monthly salary"))
+Expense = float(input("Enter your current monthly expense"))
 
+#float converts them from strings to decimals, because input only takes in strings
+
+Balance = Salary - Expense 
+
+print("your balance is: ", Balance )
